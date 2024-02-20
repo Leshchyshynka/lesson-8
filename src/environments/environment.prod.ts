f813621813349550b40c02419ccb56ca46340c61
Leshchyshynka <nadiia.leshchyshyn.sa.2020@lpnu.ua>
2024-02-20 03:51:31 +0200

    lesson 8

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyB3YuiswCh74nNaNqtxVPX8kZXtj7iKzJQ",
+    authDomain: "lesson-8-4e0af.firebaseapp.com",
+    projectId: "lesson-8-4e0af",
+    storageBucket: "lesson-8-4e0af.appspot.com",
+    messagingSenderId: "590798674146",
+    appId: "1:590798674146:web:0be0cf4c73a381462826bb"
+  },
+  production: true,
+  BACKEND_URL: 'http://localhost:3000'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyB3YuiswCh74nNaNqtxVPX8kZXtj7iKzJQ",
+    authDomain: "lesson-8-4e0af.firebaseapp.com",
+    projectId: "lesson-8-4e0af",
+    storageBucket: "lesson-8-4e0af.appspot.com",
+    messagingSenderId: "590798674146",
+    appId: "1:590798674146:web:0be0cf4c73a381462826bb"
+  },
+  production: false,
+  BACKEND_URL: 'http://localhost:3000',
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
